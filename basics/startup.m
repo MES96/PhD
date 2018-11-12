7a93f1da554d17e308eace871713f4705670da28
MES96 <32545063+MES96@users.noreply.github.com>
2018-11-12 11:34:35 +0000

    Toolbox locations update
    Updated path locations to use the toolboxes on the NAS. Enables all bugs from the scripts to be corrected by Chris without having to worry about version control.

---
+++ basics/startup.m
@@ -0,0 +1,5 @@
+% Add required toolboxes to Matlab path
+restoredefaultpath
+addpath Q:\Toolboxes\eeglab14_1_1b %path to eeglab on NAS
+addpath Q:\Toolboxes\fieldtrip-20180320 %path to fieldtrip on NAS
+ft_defaults %sets defaults and configures minimal required path settings for fieldtrip 
